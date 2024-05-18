a0b6dbf36ba65da54024fe829ad66244192b44cc
Theron Wang <doritocat2015@gmail.com>
2024-05-18 10:57:02 -0400

    Boilerplate

--- MHHackathon2024/MHHackathon2024/Program.cs
+++ MHHackathon2024/MHHackathon2024/Program.cs
@@ -1,12 +1,47 @@
-using MHHackathon2024.Client.Pages;
+using DataAccess.Data;
+using DataAccess.Models;
 using MHHackathon2024.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
+    .AddInteractiveWebAssemblyComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<IDatabase, Database>();
+builder.Services.AddSingleton<IPasswordHasher, PasswordHasher>();
+builder.Services.AddSingleton<ITagData, TagData>();
+builder.Services.AddSingleton<IPostData, PostData>();
+builder.Services.AddSingleton<IUserStore<User>, UserData>();
+
+builder.Services.AddOptions();
+
+builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication();
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("Admin", policy => policy.RequireRole("Admin"));
+
+builder.Services.AddIdentityCore<User>()
+    .AddUserStore<UserData>()
+    .AddSignInManager<SignInManager<User>>();
+
+builder.Services.ConfigureApplicationCookie(options => {
+
+    options.LoginPath = "/users/sign-in";
+    options.LogoutPath = "/users/sign-out";
+    options.AccessDeniedPath = "/users/sign-in";
+    options.SlidingExpiration = true;
+    options.ExpireTimeSpan = TimeSpan.FromDays(3);
+    options.Cookie.IsEssential = true;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+});
+
 
 var app = builder.Build();
 
@@ -25,11 +60,16 @@ else
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(MHHackathon2024.Client._Imports).Assembly);
+    .AddInteractiveWebAssemblyRenderMode();
 
 app.Run();
---
+++ MHHackathon2024/MHHackathon2024/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./Components/**/*.razor"
+    ],
+  theme: {
+      extend: {
+          colors: {
+              theme: '#165f65'
+          }
+      },
+  },
+  plugins: [],
+}
+
